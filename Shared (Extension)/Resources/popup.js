287764dedadd6642d23413e808f666f203accc37
Denis <godlin086@gmail.com>
2022-10-14 17:33:23 +0300

    add react

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,16 @@
-console.log("Hello World!", browser);
+import React, { useEffect } from 'react'
+import { render, createRoot } from 'react-dom'
+
+import dayjs from 'dayjs'
+import 'dayjs/locale/ru'
+dayjs.locale('ru')
+
+const App = () => {
+    
+    
+    return (
+        <div>Hello world</div>
+    )
+}
+
+createRoot(document.getElementById('timer')).render(<App />)
