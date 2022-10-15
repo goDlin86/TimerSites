bf846ac0cc670d230dc7f3a2a590c349f7d20a32
Denis <godlin086@gmail.com>
2022-10-15 08:39:41 +0300

    add components

---
+++ Shared (Extension)/Resources/components/TimeView.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+import 'dayjs/locale/ru'
+import duration from 'dayjs/plugin/duration'
+
+dayjs.locale('ru')
+dayjs.extend(duration)
+
+const TimeView = ({ duration, startTime }) => {
+    const allduration = duration + (startTime ? (dayjs().valueOf() - startTime) / 1000 : 0)
+    const dur = dayjs.duration(allduration, 's')
+    const hours = dur.hours() + 24 * dur.days()
+
+    const [time, setTime] = useState(dur)
+
+    useEffect(() => {
+        setTime(dur)
+        if (startTime) {
+            const timer = setInterval(() => {
+                setTime(t => t.add(1, 's'))
+            }, 1000)
+        
+            return () => clearInterval(timer)
+        }
+    }, [allduration])
+
+    return (
+        <div className="time">
+            {allduration > 0 && <div>{(hours < 10 ? '0' + hours : hours) + time.format(':mm:ss')}</div>}
+        </div>
+    )
+}
+
+export default TimeView
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,15 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { render, createRoot } from 'react-dom'
+
+import TimeView from './components/TimeView'
 
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 dayjs.locale('ru')
 
 const App = () => {
+    const [date, setDate] = useState(dayjs())
+    const [store, setStore] = useState([])
+    const [active, setActive] = useState(null)
     
+    useEffect(() => {
+        browser.storage.local.get(['storeSites', 'activeSite'], ({ storeSites, activeSite }) => {
+            if (activeSite && Object.keys(activeSite).length !== 0) {
+                setActive(activeSite)
+                if (!storeSites[storeSites.length - 1].sites.some(s => s.host === activeSite.host))
+                    storeSites[storeSites.length - 1].sites.push({ host: activeSite.host, timer: 0 })
+            }
+            setStore(storeSites)
+        })
+    }, [])
     
     return (
-        <div>Hello world</div>
+        <>
+            {store.map(s => (
+                <>
+                    <div>{dayjs(s.date).format('D MMM')}</div>
+                    {s.sites.map(site => (
+                        <>
+                            <div>{site.host}</div>
+                            <TimeView duration={site.timer} startTime={dayjs().format('YYYY-MM-DD') === s.date && active && active.host === site.host ? active.startTime : null} />
+                        </>
+                    ))}
+                </>
+            ))}
+        </>
     )
 }
 
